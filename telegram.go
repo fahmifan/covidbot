34b725f3212f0a5ce6394e9fcb001d99e4ea40c3
fahmifan <fahmifan173@gmail.com>
2021-04-02 09:13:11 +0700

    hotfix: handle empty username

--- telegram.go
+++ telegram.go
@@ -118,13 +118,21 @@ func (t *TelegramBot) syncUpdates(updates []tgbotapi.Update) error {
 
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
-		err := t.UserService.Create(ctx, &User{
-			Username:         update.Message.Chat.UserName,
+
+		username := update.Message.Chat.UserName
+		chatID := update.Message.Chat.ID
+		if username == "" {
+			username = fmt.Sprint(chatID)
+		}
+
+		user := &User{
+			Username:         username,
 			TelegramUpdateID: fmt.Sprint(update.UpdateID),
-			ChatID:           update.Message.Chat.ID,
-		})
+			ChatID:           chatID,
+		}
+		err := t.UserService.Create(ctx, user)
 		if err != nil {
-			logrus.Error(err)
+			logrus.WithField("user", Dumps(user)).Error(err)
 			return err
 		}
 	}
